489ccabc0e6c9a2e079e1136f75f97d562541773
Hamed Ghazali <hamedg20@yahoo.com>
2021-08-06 18:22:30 +0430

    dynamic routes

--- pages/products/[productId].js
+++ pages/products/[productId].js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 
 const ProductDetail = () => {
+  const router = useRouter();
+  const productId = router.query.productId;
   return (
     <div>
-      <h1>details about products</h1>
+      <h1>details about products {productId}</h1>
     </div>
   );
 };
---
+++ pages/products/sweater.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Sweater = () => {
+  return (
+    <div>
+      <h2>sweater page</h2>
+    </div>
+  );
+};
+
+export default Sweater;
